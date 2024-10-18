61c4345c0fef42d9de0470e6cd76be2d4cecaa6b
wakanore <138709595+wakanore@users.noreply.github.com>
2024-10-18 19:51:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ cursor = connection.cursor()
 
 
 
-postgres_insert_query = """ INSERT INTO tasks (id, name, description, date_start, date_end)
-                                       VALUES (%s,%s,%s)"""
+postgres_insert_query = """ INSERT INTO Project (ID, Name, Description, Start_date, End_date)
+                                       VALUES (%s,%s,%s,%s,%s)"""
 
 class STaskAdd(BaseModel):
     id: int
@@ -44,4 +44,12 @@ async def add_project(task:Annotated[STaskAdd, Depends()],):
     cursor.execute(postgres_insert_query, project_add)
     connection.commit()
     tasks.append(task)
+    return {"ok":True}
+
+
+@app.delete("/delete_project")
+async def delete_project(id):
+    postgres_delete_query = f"DELETE FROM Project WHERE id = '{id}'"
+    cursor.execute(postgres_delete_query)
+    connection.commit()
     return {"ok":True}
