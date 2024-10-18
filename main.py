442c5d6b36d62bf162d475b422d3ba6127ccf9d5
wakanore <138709595+wakanore@users.noreply.github.com>
2024-10-18 20:05:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,10 @@ cursor = connection.cursor()
 postgres_insert_query = """ INSERT INTO Project (ID, Name, Description, Start_date, End_date)
                                        VALUES (%s,%s,%s,%s,%s)"""
 
-class STaskAdd(BaseModel):
+postgres_insert_task = """ INSERT INTO Task (ID, Name, Description, Start_date, End_date, id_project)
+                                       VALUES (%s,%s,%s,%s,%s,%s)"""
+
+class SProjectAdd(BaseModel):
     id: int
     name: str
     description: Optional[str] = None
@@ -36,14 +39,14 @@ class STaskAdd(BaseModel):
     date_end: Optional[datetime] = None
 
 
-tasks = []
+projects = []
 
 @app.post("/add_project")
-async def add_project(task:Annotated[STaskAdd, Depends()],):
-    project_add = (task.id, task.name, task.description, task.date_start, task.date_end)
+async def add_project(project:Annotated[SProjectAdd, Depends()],):
+    project_add = (project.id, project.name, project.description, project.date_start, project.date_end)
     cursor.execute(postgres_insert_query, project_add)
     connection.commit()
-    tasks.append(task)
+    projects.append(project)
     return {"ok":True}
 
 
@@ -53,3 +56,29 @@ async def delete_project(id):
     cursor.execute(postgres_delete_query)
     connection.commit()
     return {"ok":True}
+
+class STaskAdd(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    date_start: Optional[datetime] = None
+    date_end: Optional[datetime] = None
+    id_project: int
+
+tasks = []
+
+@app.post("/add_task")
+async def add_task(task:Annotated[STaskAdd, Depends()],):
+    task_add = (task.id, task.name, task.description, task.date_start, task.date_end, task.id_project)
+    cursor.execute(postgres_insert_task, task_add)
+    connection.commit()
+    tasks.append(task)
+    return {"ok":True}
+
+@app.delete("/delete_task")
+async def delete_task(id):
+    postgres_delete = f"DELETE FROM Task WHERE id = '{id}'"
+    cursor.execute(postgres_delete)
+    connection.commit()
+    return {"ok":True}
+
