7bbc0602275eedefce6caf68cd255cc53248c5d6
wakanore <138709595+wakanore@users.noreply.github.com>
2024-10-27 14:46:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="PlanPro")
 connection = psycopg2.connect(
             database="PlanPro",
             user='postgres',
-            password='password',
+            password='5678',
             host='127.0.0.1',
             port='5434',
         )
@@ -25,11 +25,12 @@ cursor = connection.cursor()
 
 
 
-postgres_insert_query = """ INSERT INTO Project (ID, Name, Description, Start_date, End_date)
-                                       VALUES (%s,%s,%s,%s,%s)"""
 
-postgres_insert_task = """ INSERT INTO Task (ID, Name, Description, Start_date, End_date, id_project)
+postgres_insert_query = """ INSERT INTO Project (ID, Name, Description, Start_date, End_date, done)
                                        VALUES (%s,%s,%s,%s,%s,%s)"""
+
+postgres_insert_task = """ INSERT INTO Task (ID, Name, Description, Start_date, End_date, id_project, done)
+                                       VALUES (%s,%s,%s,%s,%s,%s,%s)"""
 
 postgres_insert_user = """ INSERT INTO User_ (ID, Name, Description, Phone_number)
                                        VALUES (%s,%s,%s,%s)"""
@@ -40,13 +41,18 @@ class SProjectAdd(BaseModel):
     description: Optional[str] = None
     date_start: Optional[datetime] = None
     date_end: Optional[datetime] = None
+    done: Optional[bool] = False
+
+class SPUTProject(BaseModel):
+    id: int
+    done: Optional[bool] = False
 
 
 projects = []
 
 @app.post("/add_project")
 async def add_project(project:Annotated[SProjectAdd, Depends()],):
-    project_add = (project.id, project.name, project.description, project.date_start, project.date_end)
+    project_add = (project.id, project.name, project.description, project.date_start, project.date_end, project.done)
     cursor.execute(postgres_insert_query, project_add)
     connection.commit()
     projects.append(project)
@@ -60,6 +66,14 @@ async def delete_project(id):
     connection.commit()
     return {"ok":True}
 
+@app.put("/update_done_project")
+async def update_done_project(project:Annotated[SPUTProject, Depends()],):
+    postgres_update_project = f"UPDATE Project SET done = '{project.done}' WHERE id = '{project.id}' "
+    cursor.execute(postgres_update_project)
+    connection.commit()
+    return {"ok": True}
+
+
 class STaskAdd(BaseModel):
     id: int
     name: str
@@ -67,12 +81,17 @@ class STaskAdd(BaseModel):
     date_start: Optional[datetime] = None
     date_end: Optional[datetime] = None
     id_project: int
+    done: Optional[bool] = False
+
+class SPUTTask(BaseModel):
+    id: int
+    done: Optional[bool] = False
 
 tasks = []
 
 @app.post("/add_task")
 async def add_task(task:Annotated[STaskAdd, Depends()],):
-    task_add = (task.id, task.name, task.description, task.date_start, task.date_end, task.id_project)
+    task_add = (task.id, task.name, task.description, task.date_start, task.date_end, task.id_project, task.done)
     cursor.execute(postgres_insert_task, task_add)
     connection.commit()
     tasks.append(task)
@@ -84,6 +103,13 @@ async def delete_task(id):
     cursor.execute(postgres_delete)
     connection.commit()
     return {"ok":True}
+
+@app.put("/update_done_task")
+async def update_done_task(task:Annotated[SPUTTask, Depends()],):
+    postgres_update_query = f"UPDATE task SET done = '{task.done}' WHERE id = '{task.id}' "
+    cursor.execute(postgres_update_query)
+    connection.commit()
+    return {"ok": True}
 
 class SUser(BaseModel):
     id: int
@@ -107,4 +133,5 @@ async def delete_user(id):
     cursor.execute(postgres_delete)
     connection.commit()
     return {"ok":True}
+
 
