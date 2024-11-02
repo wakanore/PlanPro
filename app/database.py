af3c1d043c753df5fcf4683d767e35570d51c9b7
wakanore <138709595+wakanore@users.noreply.github.com>
2024-11-02 14:49:38 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, declared_attr
-
+from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
 
 DB_HOST = '127.0.0.1'
 DB_PORT = '5434'
@@ -12,6 +12,12 @@ DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
 
 
 class Base(AsyncAttrs, DeclarativeBase):
