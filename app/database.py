35028b94186e985d15c3357f0ecc45a77b071ae5
wakanore <138709595+wakanore@users.noreply.github.com>
2024-11-04 21:54:19 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,24 +1,55 @@
-from sqlalchemy import func
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric, CheckConstraint, Boolean
+from datetime import datetime
+
+metadata = MetaData()
+
+DB_USER= 'postgres'
+DB_PASSWORD='5678'
+DB_HOST='127.0.0.1'
+DB_PORT='5434'
+DB_NAME="PlanPro"
+
+DATABASE_URL = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_engine(DATABASE_URL)
 
 
 
-DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-int_pk = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[datetime, mapped_column(server_default=func.now())]
-updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
-str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
-str_null_true = Annotated[str, mapped_column(nullable=True)]
+Project = Table('Project', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(100)),
+    Column('description', String(1000)),
+    Column('done', Boolean()),
+    Column('start_date', DateTime(), default=datetime.now),
+    Column('end_date', DateTime())
+)
 
 
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True
+Task = Table('Task', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(200), nullable=False),
+    Column('start_date', DateTime(), default=datetime.now),
+    Column('end_date', DateTime()),
+    Column('description', String(1000)),
+    Column('done', Boolean()),
+    Column('id_project', Integer(), ForeignKey('Project.id'))
+)
 
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return f"{cls.__name__.lower()}s"
+
+Users = Table('Users', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(100)),
+    Column('phone_number', String(20)),
+    Column('description', String(1000))
+
+)
+
+
+Users_projects = Table('Users_projects', metadata,
+    Column('id_project', ForeignKey('Project.id')),
+    Column('id_user', ForeignKey('Users.id'))
+)
+
+
+metadata.create_all(engine)
