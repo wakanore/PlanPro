b72125c0cbc2e16915f07eb043d21801d02817c7
wakanore <138709595+wakanore@users.noreply.github.com>
2024-11-10 14:34:40 +0300

    commit

---
+++ app/users/auth.py
@@ -0,0 +1,43 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+from app.config import get_auth_data
+from fastapi import APIRouter, HTTPException, status
+from app.users.dao import UsersDAO
+from app.users.schemas import SUserRegister
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=30)
+    to_encode.update({"exp": expire})
+    auth_data = get_auth_data()
+    encode_jwt = jwt.encode(to_encode, auth_data['secret_key'], algorithm=auth_data['algorithm'])
+    return encode_jwt
+
+
+router = APIRouter(prefix='/auth', tags=['Auth'])
+
+
+@router.post("/register/")
+async def register_user(user_data: SUserRegister) -> dict:
+    user = await UsersDAO.find_one_or_none(email=user_data.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Пользователь уже существует'
+        )
+    user_dict = user_data.dict()
+    user_dict['password'] = get_password_hash(user_data.password)
+    await UsersDAO.add(**user_dict)
+    return {'message': 'Вы успешно зарегистрированы!'}
