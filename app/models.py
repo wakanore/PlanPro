5546dd71572ddbea7951bfd9c3cc416236907515
wakanore <138709595+wakanore@users.noreply.github.com>
2024-11-13 18:45:37 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -46,7 +46,6 @@ class SUser(BaseModel):
 
 
 class SUserRegister(BaseModel):
-    email: str = Field(..., description="Электронная почта")
     password: str = Field(..., min_length=5, max_length=50, description="Пароль, от 5 до 50 знаков")
     phone_number: str = Field(..., description="Номер телефона в международном формате, начинающийся с '+'")
     first_name: str = Field(..., min_length=3, max_length=50, description="Имя, от 3 до 50 символов")
