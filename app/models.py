1cb04d3b5b0b881ff54c4cf4bac92bd2c2d88740
wakanore <138709595+wakanore@users.noreply.github.com>
2024-11-17 19:27:34 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel, Field, field_validator
 from datetime import  datetime
 import re
 
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base
+
 
 class SProjectAdd(BaseModel):
     id: int
@@ -48,6 +51,7 @@ class SUserRegister(BaseModel):
     password: str = Field(..., min_length=5, max_length=50, description="Пароль, от 5 до 50 знаков")
     phone_number: str = Field(..., description="Номер телефона в международном формате, начинающийся с '+'")
     name: str = Field(..., min_length=3, max_length=50, description="Имя, от 3 до 50 символов")
+    description: Optional[str] = None
 
     @field_validator("phone_number")
     @classmethod
@@ -55,6 +59,57 @@ class SUserRegister(BaseModel):
         if not re.match(r'^\+\d{5,15}$', values):
             raise ValueError('Номер телефона должен начинаться с "+" и содержать от 5 до 15 цифр')
         return values
+
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
 
 
 
