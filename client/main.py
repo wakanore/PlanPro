cf8efbcd64220c5cee1096bcaa1c0387cf50c44d
wakanore <138709595+wakanore@users.noreply.github.com>
2024-12-18 18:57:14 +0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -11,10 +11,15 @@ from pydantic import BaseModel, Field
 app = FastAPI()
 
 
-class User(BaseModel):
-    id: int
+class UserLogin(BaseModel):
+    phone_number: str
+    password: str
+
+class UserRegistr(BaseModel):
+    password: str
+    phone_number: str
     name: str
-    dob: date = Field(title='Date of Birth')
+    description: Optional[str] = None
 
 class Project(BaseModel):
     id: int
@@ -23,6 +28,19 @@ class Project(BaseModel):
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
     done: Optional[bool] = False
+
+class Task(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+    id_project: int
+    done: Optional[bool] = False
+
+tasks = [
+    Task(id=1, name='n', description='d', start_date=date(1990, 1, 1), end_date=date(1991, 1, 1), id_project=1, done=False),
+]
 
 
 projes = [
@@ -34,12 +52,7 @@ projes = [
 
 
 # define some users
-users = [
-    User(id=1, name='John', dob=date(1990, 1, 1)),
-    User(id=2, name='Jack', dob=date(1991, 1, 1)),
-    User(id=3, name='Jill', dob=date(1992, 1, 1)),
-    User(id=4, name='Jane', dob=date(1993, 1, 1)),
-]
+
 
 
 @app.get("/api/", response_model=FastUI, response_model_exclude_none=True)
@@ -52,7 +65,7 @@ def users_table() -> list[AnyComponent]:
                     data=projes,
                     columns=[
                         DisplayLookup(field='name', on_click=GoToEvent(url='/user/{id}/')),
-                        DisplayLookup(field='done', mode=DisplayMode.date),
+                        DisplayLookup(field='done', on_click=GoToEvent(url='/login')),
                     ],
                 ),
             ]
@@ -72,10 +85,49 @@ def user_profile(user_id: int) -> list[AnyComponent]:
                 c.Heading(text=user.name, level=2),
                 c.Link(components=[c.Text(text='Back')], on_click=BackEvent()),
                 c.Details(data=user),
+                c.Table(
+                    data=tasks,
+                    columns=[
+                        DisplayLookup(field='name', on_click=GoToEvent(url='/registration')),
+                        DisplayLookup(field='description', mode=DisplayMode.date),
+                        DisplayLookup(field='start_date', mode=DisplayMode.date),
+                        DisplayLookup(field='end_date', mode=DisplayMode.date),
+                        DisplayLookup(field='done', mode=DisplayMode.date),
+                    ],
+                ),
             ]
         ),
     ]
 
+
+@app.get("/api/login", response_model=FastUI, response_model_exclude_none=True)
+def get_upload_data_page():
+    return [
+        c.Page(
+            components=[
+                c.Heading(text='Login', level=2),
+                c.ModelForm(
+                    model=UserLogin,
+                    submit_url='/api/login',
+                ),
+                c.Button(text='Registration', on_click=GoToEvent(url='/registration'))
+            ]
+        ),
+    ]
+
+@app.get("/api/registration", response_model=FastUI, response_model_exclude_none=True)
+def get_upload_data_page():
+    return [
+        c.Page(
+            components=[
+                c.Heading(text='Registration', level=2),
+                c.ModelForm(
+                    model=UserRegistr,
+                    submit_url='/api/registration',
+                )
+            ]
+        ),
+    ]
 
 @app.get('/{path:path}')
 async def html_landing() -> HTMLResponse:
