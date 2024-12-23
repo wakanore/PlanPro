1f833a82cfb449a168a616b1eec48634aeb15f84
wakanore <138709595+wakanore@users.noreply.github.com>
2024-12-23 17:05:18 +0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,58 +1,14 @@
 from datetime import date, datetime
 from typing import Optional
-
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from fastui import FastUI, AnyComponent, prebuilt_html, components as c
 from fastui.components.display import DisplayMode, DisplayLookup
 from fastui.events import GoToEvent, BackEvent
 from pydantic import BaseModel, Field
+from client.models import Task, Project, ProjectADD, TaskAdd, NewUser, UserLogin, UserRegistr
 
 app = FastAPI()
-
-
-class UserLogin(BaseModel):
-    phone_number: str
-    password: str
-
-class UserRegistr(BaseModel):
-    password: str
-    phone_number: str
-    name: str
-    description: Optional[str] = None
-
-class Project(BaseModel):
-    id: int
-    name: str
-    description: Optional[str] = None
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
-    done: Optional[bool] = False
-
-class Task(BaseModel):
-    id: int
-    name: str
-    description: Optional[str] = None
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
-    id_project: int
-    done: Optional[bool] = False
-
-class ProjectADD(BaseModel):
-    name: str
-    description: Optional[str] = None
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
-
-class NewUser(BaseModel):
-    phone_number: str
-
-class TaskAdd(BaseModel):
-    name: str
-    description: Optional[str] = None
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
-    id_project: int
 
 tasks = [
     Task(id=1, name='n', description='d', start_date=date(1990, 1, 1), end_date=date(1991, 1, 1), id_project=1, done=False),
@@ -68,14 +24,13 @@ projes = [
 ]
 
 
-# define some users
 
-
-
+#главная страница
 @app.get("/api/", response_model=FastUI, response_model_exclude_none=True)
 def users_table() -> list[AnyComponent]:
     return [
         c.Page(
+            #таблица проектов
             components=[
                 c.Heading(text='Projects', level=2),
                 c.Table(
@@ -85,7 +40,9 @@ def users_table() -> list[AnyComponent]:
                         DisplayLookup(field='done', on_click=GoToEvent(url='/login')),
                     ],
                 ),
+                #заголовок
                 c.Heading(text='Add Project', level=2),
+                #добавление проекта
                 c.ModelForm(
                     model=ProjectADD,
                     submit_url='/api/registration',
@@ -95,6 +52,7 @@ def users_table() -> list[AnyComponent]:
     ]
 
 
+#описание проекта
 @app.get("/api/user/{user_id}/", response_model=FastUI, response_model_exclude_none=True)
 def user_profile(user_id: int) -> list[AnyComponent]:
     try:
@@ -110,17 +68,20 @@ def user_profile(user_id: int) -> list[AnyComponent]:
                 c.Details(data=user),
                 c.Button(text='Done', on_click=GoToEvent(url='/api/')),
                 c.Button(text='Delete', on_click=GoToEvent(url='/api/')),
+                #просмотр задач
                 c.Table(
                     data=projes,
                     columns=[
                         DisplayLookup(field='name', on_click=GoToEvent(url='/tasks/{id}/')),
                     ],
                 ),
+                #добавление задачи
                 c.Heading(text='Add Task', level=3),
                 c.ModelForm(
                     model=TaskAdd,
                     submit_url='/api/registration',
                 ),
+                #добавление пользователя
                 c.Heading(text='Add User', level=3),
                 c.ModelForm(
                     model=NewUser,
@@ -131,6 +92,7 @@ def user_profile(user_id: int) -> list[AnyComponent]:
     ]
 
 
+#описание задач
 @app.get("/api/tasks/{id_project}/", response_model=FastUI, response_model_exclude_none=True)
 def user_profile(id_project: int) -> list[AnyComponent]:
     try:
@@ -140,6 +102,7 @@ def user_profile(id_project: int) -> list[AnyComponent]:
     return [
         c.Page(
             components=[
+                #таблица задач
                 c.Heading(text='Tasks', level=2),
                 c.Link(components=[c.Text(text='Back')], on_click=BackEvent()),
                 c.Table(
@@ -160,7 +123,7 @@ def user_profile(id_project: int) -> list[AnyComponent]:
     ]
 
 
-
+#вход пользователя
 @app.get("/api/login", response_model=FastUI, response_model_exclude_none=True)
 def get_upload_data_page():
     return [
@@ -178,7 +141,7 @@ def get_upload_data_page():
     ]
 
 
-
+#регистрация
 @app.get("/api/registration", response_model=FastUI, response_model_exclude_none=True)
 def get_upload_data_page():
     return [
