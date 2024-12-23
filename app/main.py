e01de4a79ade7c80ac6b65a5ec3b11cce52d2f8b
wakanore <138709595+wakanore@users.noreply.github.com>
2024-12-23 16:59:49 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,25 +2,6 @@ from fastapi import FastAPI
 from app.routers import router as router_students
 
 
-
-
 app = FastAPI(title="PlanPro")
 
 app.include_router(router_students)
-
-
-
-<<<<<<< HEAD
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to FastAPI authentication and authorization example"}
-
-
-
-
-
-
-
-=======
->>>>>>> 06bd7fd2e8abd184b5468c9f06afe30b9d8f26c5
